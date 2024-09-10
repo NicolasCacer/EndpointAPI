3e95ae064000b316b909fe4157a333c10abceead
Nicolas <njcaceres04@gmail.com>
2024-09-10 08:00:16 -0500

    fuctional api

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ import data from './data.json' assert { type: "json" };
 
 const app = express();
 
-app.get("/user-info",(req, res)=>{
-    res.json(data);
+app.get("/user-info/:id?",(req, res)=>{
+    const id = req.params.id;
+    if (!id) {
+        return res.json(data);
+    } else if (typeof(parseInt(id,10))=="number"){
+        res.json(data[parseInt(id,10)]);
+    }
 });
 
-app.listen(3000, ()=> console.log(`Api server is running at http://localhost:${3000}/user-info`));
+app.listen(3000, ()=> console.log(`Api server is running at http://localhost:${3000}/user-info/1`));
